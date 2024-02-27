a459ec37a32633a5f5917421b01952fb06e5a513
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-27 17:30:52 +0300

    Update A-003-SamplingTheory.py

--- A-003-SamplingTheory.py
+++ A-003-SamplingTheory.py
@@ -1,7 +1,6 @@
 #let's look at population and sample means
 
 #our_population.mean()
+print(f"Population mean: {np.mean(our_population)}") # Population mean: 44.5015
 
-print(f"Population mean: {np.mean(our_population)}") # Population mean: 44.524
-
-print(f"Sample mean: {np.mean(sample_people)}") # Sample mean: 46.11
+print(f"Sample mean: {np.mean(sample_people)}") # Sample mean: 47.28
