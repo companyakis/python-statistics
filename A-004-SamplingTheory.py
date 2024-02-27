a8eb013a174f6af6751e5682bd93f131ba1a8062
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-27 17:35:46 +0300

    Create A-004-SamplingTheory.py

---
+++ A-004-SamplingTheory.py
@@ -0,0 +1,44 @@
+#let's create diff sample groups
+
+np.random.seed(142)
+
+sample1 = np.random.choice(our_population, 100)
+
+sample2 = np.random.choice(our_population, 100)
+
+sample3 = np.random.choice(our_population, 100)
+
+sample4 = np.random.choice(our_population, 100)
+
+sample5 = np.random.choice(our_population, 100)
+
+sample6 = np.random.choice(our_population, 100)
+
+sample7 = np.random.choice(our_population, 100)
+
+sample8 = np.random.choice(our_population, 100)
+
+sample9 = np.random.choice(our_population, 100)
+
+sample10 = np.random.choice(our_population, 100)
+
+print(sample1)
+
+print("-------------------------------------------------------------------------")
+
+print(sample10)
+
+"""
+[88 29 20 71 77 59  8 47  6 21 50 55 78 77 59 50 10 73 38 44 72 46 13 79
+ 74 37 20 36 66 62 63 80 56  6 56 15 30 52 45 47 83 39 11 68  4 16 70 56
+ 85 48 74 59 52 72 63 51 65 41  4 79 65 42 57  5 70 33 54 14 43 79  5 66
+ 72 33 20 60 81 17 66 38 22 19 62 58 59 83 47 58 15 23 40 60 20 32 85 43
+ 14 25 67 61]
+-------------------------------------------------------------------------
+[17  6  1 78 67 85 80 72 57 23 11 49 83 30 30 42 13 56 10  5 74  8 82 23
+ 20 66 54 68 49 59 77 10 30 76 33 49 82  0 16 64 36 31 25 89 75 66 53 27
+ 44 30 13 20 35  0 54 29 21 12 21 19 27 76  3 27 24 17 14 24 30 38 71 14
+ 74 67 56 30 54 83 82 57 69 81 21 80 40 40  6 49  2 78 13 41 70 41 32 17
+ 70 28  0 37]
+
+"""
