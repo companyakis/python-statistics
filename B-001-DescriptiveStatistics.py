255c7f45b6053cb039c393b594dfb15d28cfbdf4
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-29 00:02:15 +0300

    Create B-001-DescriptiveStatistics.py

---
+++ B-001-DescriptiveStatistics.py
@@ -0,0 +1,28 @@
+import seaborn as sns
+import pandas as pd
+
+data_tips = sns.load_dataset("tips")
+
+print(data_tips.head())
+
+print(data_tips.describe())
+
+"""
+   total_bill   tip     sex smoker  day    time  size
+0       16.99  1.01  Female     No  Sun  Dinner     2
+1       10.34  1.66    Male     No  Sun  Dinner     3
+2       21.01  3.50    Male     No  Sun  Dinner     3
+3       23.68  3.31    Male     No  Sun  Dinner     2
+4       24.59  3.61  Female     No  Sun  Dinner     4
+
+       total_bill         tip        size
+count  244.000000  244.000000  244.000000
+mean    19.785943    2.998279    2.569672
+std      8.902412    1.383638    0.951100
+min      3.070000    1.000000    1.000000
+25%     13.347500    2.000000    2.000000
+50%     17.795000    2.900000    2.000000
+75%     24.127500    3.562500    3.000000
+max     50.810000   10.000000    6.000000
+
+"""
